045a8b799907beb1c3d77c720570c09f60afe26d
Vinicius Machado <vinnikb100@gmail.com>
2024-01-22 11:23:32 -0300

    Service layer, component registration

---
+++ src/main/java/com/projetocurso/course/services/UserService.java
@@ -0,0 +1,29 @@
+package com.projetocurso.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetocurso.course.entities.User;
+import com.projetocurso.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+		// método para retornar todos os usuários do banco de dados.
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();// a operação get do optional retorna o objeto do tipo que está dentro de < > que está dentro do optional.
+		// método para recuperar um usuário por Id
+		
+	}
+}
