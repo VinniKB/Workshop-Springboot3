583add29a384645f5e77ea9e508b1eb87601d8ee
Vinicius Machado <vinnikb100@gmail.com>
2024-01-24 16:32:07 -0300

    Exception handling - update

--- src/main/java/com/projetocurso/course/services/UserService.java
+++ src/main/java/com/projetocurso/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetocurso.course.repositories.UserRepository;
 import com.projetocurso.course.services.exceptions.DatabaseException;
 import com.projetocurso.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -45,9 +47,13 @@ public class UserService {
 		}
 	}
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
